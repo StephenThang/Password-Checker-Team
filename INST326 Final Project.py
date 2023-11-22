556b379ec88e46956ceeb2a85e9a4edae66ccf06
StephenThang <65163691+StephenThang@users.noreply.github.com>
2023-11-21 23:28:45 -0500

    Update INST326 Final Project.py

--- INST326 Final Project.py
+++ INST326 Final Project.py
@@ -9,12 +9,10 @@ Password checker function that check if password meets the length requirements (
 
 return: length of password typed or an error message
 """
-def password_length_checker(password):
+def is_password_long_enough(password):
     min_length = 8 
-    if password >= min_length:
-        return len(password)
-    else: 
-        return "Password does not meet length criteria (8 characters)"
+    return len(password) >= min_length
+        
 
 """
 Password complexity verification: checks for necessary characteristics such as uppercase, lowercase, numbers, and special characters.
@@ -22,22 +20,22 @@ Password complexity verification: checks for necessary characteristics such as u
 return: message that indicates which characteristics the password contains. 
 
 """
-def password_complexity_checker(password): 
-    upperCase = False
-    lowerCase = False
+def is_password_complex(password): 
+    upper_case = False
+    lower_case = False
     number = False
     special = False
 
     for char in password:
         if char.isupper():
-            upperCase = True
+            upper_case = True
         elif char.islower():
-            lowerCase = True
+            lower_case = True
         elif char.isdigit():
             number = True
         elif char in "!@#$%^&*()_+{}[]|\:;<>,.?/":
             special = True 
-    return f"Has uppercase {upperCase}, has lowercase {lowerCase}, has number {number},  has special character {special}"
+    return all([upper_case, lower_case, number, special])
          
 
 """
@@ -54,7 +52,7 @@ def password_suggestion_generator(password):
     has_lowercase = False
     has_uppercase = False
 
-    if p in password:
+    for p in password:
         if p.isdigit():
             has_number = True
         elif p.islower():
@@ -82,12 +80,12 @@ def text_file_analyzer(filename):
 
     for line in file:
         password = line.strip()
-        if password_length_checker(password) and password_complexity_checker(password):
+        if is_password_long_enough(password) and is_password_complex(password):
             strong_passwords.append(password)
         else:
             weak_passwords.append(password)
     file.close()
-    return f"Your strong password: {strong_passwords}, your weak password: {weak_passwords}"
+    return strong_passwords, weak_passwords
 
 """
 Ranks lists of passwords based on their length and complexity
@@ -96,6 +94,10 @@ Returns: list of passwords from strongest to least strong.
 """
 def password_ranker(passwords):
     def password_ranker_key(password):
-        return (len(password), password_complexity_checker(password))
+        return (len(password), is_password_complex(password))
     ranked_pass = sorted(passwords, key=password_ranker_key )
     return ranked_pass
+
+def input_checker():
+    password = input("Enter Password: ")
+    
