6c5c079dfd2fa160187b8984d4e0031af6d5ac3e
StephenThang <65163691+StephenThang@users.noreply.github.com>
2023-12-15 03:27:45 -0500

    password_checker_unittest.py

--- Unit Test Password Strength Checker.py
+++ Unit Test Password Strength Checker.py
@@ -1,5 +1,5 @@
 import unittest
-from INST326_Final_Project import *
+from password_checker import *
 class TestPasswordFunctions(unittest.TestCase):
 
     def test_password_length_checker(self):
@@ -14,16 +14,16 @@ class TestPasswordFunctions(unittest.TestCase):
         self.assertIn("Try a longer password", suggestions)
         # print(suggestions)
         self.assertIn("Try adding numbers to increase complexity of password", suggestions)
-        self.assertIn("Try adding uppercase to increase complexity of password", suggestions)
+        self.assertIn("Try adding upper case letters to increase complexity of password", suggestions)
 
     def test_text_file_analyzer(self):
         strong_passwords, weak_passwords = text_file_analyzer("1000-most-common-passwords.csv")
         self.assertIn("Jackie72&$", strong_passwords )
         self.assertIn("brian", weak_passwords)
 
-
     def test_password_ranker(self):
-        passwords = ["Password123", "weakpassword","strongP@ss"]
-
+        passwords = ["Password123", "weakpassword","strongP@ss33", "st*&rongP@ss87"]
+        correct_output = ["st*&rongP@ss87", "strongP@ss33","Password123", "weakpassword"]
+        self.assertEqual(password_ranker(passwords), correct_output)
 if __name__ == '__main__':
     unittest.main()
